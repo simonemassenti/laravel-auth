b72cdd168eee1cf6619a3e84e673e277364e93d8
simonemassenti <s.massenti96@gmail.com>
2024-01-24 15:10:43 +0100

    add: mutator

--- app/Models/Portfolio.php
+++ app/Models/Portfolio.php
@@ -4,10 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Portfolio extends Model
 {
     use HasFactory;
 
     protected $fillable=['title', 'description', 'slug'];
+
+
+    //Mutator che setta lo slug in base al titolo
+    public function setTitleAttribute($_title){
+        $this->attributes['title'] = $_title;
+        $this->attributes['slug'] = Str::slug($_title);
+    }
 }
